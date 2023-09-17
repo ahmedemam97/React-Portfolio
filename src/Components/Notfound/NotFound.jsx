33058eb3a40dd3f176c1158726dafb392221cc40
Ahmed Emam <ahmed.emam12380@gmail.com>
2023-09-17 21:34:47 +0300

    add not found image

--- src/Components/Notfound/NotFound.jsx
+++ src/Components/Notfound/NotFound.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import notfoundImg from '../../assets/error.svg'
 export default function NotFound() {
     return (
         <div className="container">
             <div className="row">
-            <div className="not-found">
-                <h1>Page Not Found</h1>
+            <div className="not-found text-center">
+                <h1>Page Not Found!</h1>
+                <img className='mt-4' src={notfoundImg} alt="" />
                 </div>
             </div>
         </div>
